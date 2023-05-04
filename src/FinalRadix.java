57e10f9ce951090997a108079d10a20107a2c933
supershuaifeng <limchinfeng@gmail.com>
2023-05-04 11:33:43 +0800

    Add comment to Integer Radix Sort

--- src/FinalRadix.java
+++ src/FinalRadix.java
@@ -5,58 +5,59 @@ import java.util.List;
 public class FinalRadix {
 
     public static int[] IntegerRadixsort(int[] arr) {
-        // Get the size of the input arrau
-        int arraySize = arr.length;
+        // Get the size of the input arra
+        int arrSize = arr.length;
         // Find the maximum value in the input array
         int maxValue = getMaxValue(arr);
         // Set the starting value for the exponent
 
-        int exp = 1;
+        int exponent = 1;
 
         // Create two arrays of ArrayLists to store the integers
-        List<Integer>[] array1 = createArrayLists();
-        List<Integer>[] array2 = createArrayLists();
+        List<Integer>[] Array1 = createArrayLists();
+        List<Integer>[] Array2 = createArrayLists();
 
         // Sort the input array by the least significant digit
-        for (int count1 = 0; count1 < arraySize; count1++) {
-            int index = (arr[count1] / exp) % 10;
-            int value = arr[count1];
-            array1[index].add(value);
+        for (int i = 0; i < arrSize; i++) {
+            int index = (arr[i] / exponent) % 10;
+            int value = arr[i];
+            Array1[index].add(value);
         }
 
         // Update the exponent to the next significant digit
-        exp *= 10;
+        exponent *= 10;
 
         // Repeat the process until all significant digits have been sorted
-        while (maxValue / exp > 0) {
+        while (maxValue / exponent > 0) {
 
             // Move the values from array1 to array2, sorted by the next significant digit
-            for (int count = 0; count < 10; count++) {
-                while (!array1[count].isEmpty()) {
-                    int arrValue = array1[count].remove(0);
-                    int index1 = (arrValue / exp) % 10;
-                    array2[index1].add(arrValue);
+            for (int i = 0; i < 10; i++) {
+                while (!Array1[i].isEmpty()) {
+                    int Array1Value = Array1[i].remove(0);
+                    int index1 = (Array1Value / exponent) % 10;
+                    Array2[index1].add(Array1Value);
                 }
             }
             // Update the exponent to the next significant digit
-            exp *= 10;
+            exponent *= 10;
+
             // Move the values from array2 to array1, sorted by the next significant digit
-            for (int count = 0; count < 10; count++) {
-                while (!array2[count].isEmpty()) {
-                    int arrValue2 = array2[count].remove(0);
-                    int index2 = ((arrValue2 / exp) % 10);
-                    array1[index2].add(arrValue2);
+            for (int i = 0; i < 10; i++) {
+                while (!Array2[i].isEmpty()) {
+                    int Array2Value = Array2[i].remove(0);
+                    int index2 = ((Array2Value / exponent) % 10);
+                    Array1[index2].add(Array2Value);
                 }
             }
-            exp *= 10;
+            exponent *= 10;
         }
         // Create a sorted array from the values in array1
-        int[] sortedArray = new int[arraySize];
+        int[] sortedArray = new int[arrSize];
         int arrCount = 0;
-        for (int count = 0; count < 10; count++) {
-            while (!array1[count].isEmpty()) {
-                int arrValue3 = array1[count].remove(0);
-                sortedArray[arrCount] = arrValue3;
+        for (int i = 0; i < 10; i++) {
+            while (!Array1[i].isEmpty()) {
+                int sortedArrayValue = Array1[i].remove(0);
+                sortedArray[arrCount] = sortedArrayValue;
                 arrCount++;
             }
         }
@@ -74,6 +75,7 @@ public class FinalRadix {
         return maxValue;
     }
 
+    // A method to create a arraylist inside an array
     private static List<Integer>[] createArrayLists() {
         List<Integer>[] arrays = new ArrayList[10];
         for (int i = 0; i < 10; i++) {
